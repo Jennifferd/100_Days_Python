071a0fe825748c91b870751ea84b72d2a1601095
Jenniffer  Delgado <jenniffer@ndus3.com>
2025-09-22 22:07:28 +0200

    finished day63

--- 63_Library_project/main.py
+++ 63_Library_project/main.py
@@ -63,13 +63,35 @@ def add():
         with app.app_context():
             db.create_all()
             new_book = Book(title=request.form["title"],
-                            author=request.form["title"],
+                            author=request.form["author"],
                             rating=request.form["rating"])
             db.session.add(new_book)
             db.session.commit()
         return redirect(url_for('home'))
 
     return render_template("add.html", form=form)
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get("id")
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit.html", book=book_selected)
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get("id")
+    book_to_delete = db.get_or_404(Book, book_id)
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
